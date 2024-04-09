6bc386cf445172bba8b6fe4f6674b056a3924003
dayann254 <157493227+dayann254@users.noreply.github.com>
2024-04-09 13:06:52 +0300

    Create Ranges.kt

---
+++ Ranges.kt
@@ -0,0 +1,30 @@
+//fun main(args: Array<String>, other: Int, to: Int) {
+//    println("Number Range: ")
+//
+//    for(num in 1..5){
+//        println(num)
+//    }
+//    println("Character Range:  ")
+//
+//    for (char in 'A'..'E')
+//        println(char)
+//    var number=1.rangeTo(other: 5)
+//    println("rangeTo:")
+//    for(x in number){
+//        println(x)
+//    }
+////    using down to()insteadn of '..'
+//    var namba = 10.downTo(to:1)
+//    println("Down to:")
+//    for(y in namba){
+//        println(y)
+//    }
+//    var num1 = 1.rangeTo(other:10)
+//    var oddNumber = num1.step(3)
+//    for(n in oddNumber)
+//        println(n)
+//
+//    val OnetoSix = 1..6
+//    println("3 in OnetoSix:${3 in OnetoSix}")
+//    println("7 in OnetoSix:${3 in OnetoSix}")
+//}
